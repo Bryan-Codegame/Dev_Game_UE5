6869dea592f5ecf7e937ae8891c6d9e277e31560
Bryan Escobar <bryanesc26@gmail.com>
2023-04-08 02:34:36 -0600

    First commit

---
+++ Source/DevGame/DevGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DevGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADevGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADevGameGameMode();
+};
+
+
+
