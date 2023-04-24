d4a834977cb1e2e962fe94dac10b48230a805dbe
Bryan Escobar <bryanesc26@gmail.com>
2023-04-23 18:33:26 -0600

    SlugEnemy UPROPERTY explain

--- Source/DevGame/Public/SlugEnemy.h
+++ Source/DevGame/Public/SlugEnemy.h
@@ -23,4 +23,31 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	UFUNCTION()
+		void OnHit(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit);
+
+	// Any Object that has at least one valid pointer stored inside a UPROPERTY will not be Garbage Collected (GC). 
+	// Non-UPROPERTY variables will not be counted by the GC system.
+	// - https://wiki.unrealengine.com/UPROPERTY#Garbage_Collection
+	// It cannot be declared as a TSharedPtr; UPROPERTIES are smart pointers implicitly, and so they follow another life cycle
+
+	//We use UPROPERTY to free memory with the Garbage Collected
+	UPROPERTY()
+	TWeakObjectPtr<USkeletalMeshComponent> SkeletalMesh;
+	UPROPERTY()
+	TWeakObjectPtr<UAnimSequence> IdleAnimation;
+	UPROPERTY()
+	TWeakObjectPtr<UParticleSystem> ExplosionParticleSystem;
+
+	// Template it to allow UClass's to be passed around with type safety instead of using UClass*
+	TSubclassOf<AActor> ProjectileClass;
+
+	// A reference to the main player for enemies to look at him
+	TWeakObjectPtr<APawn> PlayerPawn;
+
+	float AccumulatedDeltaTime;
+
+	float const FireTimeInterval = 2.0f;
+
 };
