7f46735512fe8f7041ac1a84fdbc6f2e1b2fdfe4
Bryan Escobar <bryanesc26@gmail.com>
2023-05-01 22:57:10 -0600

    Add Abstract and Pure virtual function macros BaseEnemy.h

--- Source/DevGame/Public/BaseEnemy.h
+++ Source/DevGame/Public/BaseEnemy.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "BaseEnemy.generated.h"
 
-UCLASS()
+UCLASS(Abstract)
 class DEVGAME_API ABaseEnemy : public AActor
 {
 	GENERATED_BODY()
@@ -23,4 +23,28 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	UPROPERTY()
+	TWeakObjectPtr<UAnimSequence> IdleAnimation;
+	UPROPERTY()
+	TWeakObjectPtr<UParticleSystem> ExplosionParticleSystem;
+
+protected:
+	UFUNCTION()
+	virtual void OnHit(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit);
+
+	// (2) 
+	// ... so, in order to create pure virtual functions (i.e., functions that
+	// must be implemented in child classes), UE4 provides the PURE_VIRTUAL(function, default_code)
+	// macro. Watch out for the mandatory comma!
+	virtual void RunBehaviour() PURE_VIRTUAL(ABaseEnemy::RunBehaviour, );
+
+	UPROPERTY()
+	TWeakObjectPtr<USkeletalMeshComponent> SkeletalMesh;
+
+	TSubclassOf<AActor> ProjectileClass;
+	TWeakObjectPtr<APawn> PlayerPawn;
+	float AccumulatedDeltaTime;
+
+	float const FireTimeInterval = 2.0f;
 };
